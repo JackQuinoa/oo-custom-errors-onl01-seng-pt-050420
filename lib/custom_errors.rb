4955d5eefe8da1e1a353e610d533bb6222546b09
Jack Quinoa <jquinoa208@gmail.com>
2020-06-10 20:06:31 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,6 +9,10 @@ class Person
     self.partner = person
     person.partner = self
   end
+  
+  class PartnerError < StandardError
+
+  end
 
 end
 
@@ -16,7 +20,5 @@ beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
-class PartnerError < StandardError
 
-end
 
